1b41cd30fb7671fe3e3efbe03b06e19250a694fd
Adam Cooper <adam@networkpie.co.uk>
2018-11-09 14:50:46 +0000

    Homogenised the handling of images that should get a lightbox i.e. all of them pretty much

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,7 @@ import Gallery from './javascript/gallery.js';
 
 const nav = new NavBar(document.querySelector('nav.is-fixed-top'));
 
-var instaImages = Array.prototype.slice.call(document.querySelectorAll('.post .post-image'), 0);
-instaImages.forEach(imageDiv => {
+var postImages = Array.prototype.slice.call(document.querySelectorAll('article.post .post-image, article.post .post-body'), 0);
+postImages.forEach(imageDiv => {
     var image = new Gallery(imageDiv);
-});
-
-var galleries = Array.prototype.slice.call(document.querySelectorAll('div.gallery'), 0);
-galleries.forEach(galleryDiv => {
-    var gallery = new Gallery(galleryDiv);
 });
